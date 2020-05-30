26adb001ed321af753a096f21a6efdf22baf5689
niko.pan <niko.pan@homecredit.cn>
2020-05-30 11:08:35 +0800

    add payment module

---
+++ wink-springcloud-payment/src/main/java/cn/niko/wink/springcloud/PaymentApplication.java
@@ -0,0 +1,17 @@
+package cn.niko.wink.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author niko.pan
+ * @version 1.0.0
+ * @since 2020/5/29 23:36
+ */
+@SpringBootApplication
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class);
+    }
+}
