679a47bf78222524580111cfecc35c9da7b7df0d
niko.pan <niko.pan@homecredit.cn>
2020-05-30 15:23:21 +0800

    add eureka module

---
+++ wink-springcloud-eureka-node7002/src/main/java/cn/niko/wink/springcloud/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.niko.wink.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author niko.pan
+ * @version 1.0.0
+ * @since 2020/5/30 12:28
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
